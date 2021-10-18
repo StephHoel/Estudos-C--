116cb01e631417c6a917c2b348c87f21b7b90121
StephHoel <steph.hoel@gmail.com>
2021-10-18 13:32:08 -0300

    Aula 3 - Arquivo Base

---
+++ aula3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <locale.h> // setlocale
+#include <stdlib.h> // system
+#include <string>   // Textos em geral
+
+using namespace std;
+
+
+
+int main()
+{
+   setlocale(LC_ALL, "Portuguese");
+
+
+
+   cout << "\n\n";
+   system("pause");
+   return 0;
+}
+
